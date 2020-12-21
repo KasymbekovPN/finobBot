6e277fd1d95a1fbbc9953d5336157a23aaa17920
KasymbekovPN <kasymbekovPN@yandex.ru>
2020-12-21 22:39:38 +0300

    getting of bot conf is changed

--- finobBot-IO/src/main/java/org/kpn/finobbot_io/config/BotConfig.java
+++ finobBot-IO/src/main/java/org/kpn/finobbot_io/config/BotConfig.java
@@ -1,9 +1,11 @@
 package org.kpn.finobbot_io.config;
 
 import org.kpn.finobbot_io.Bot;
+import org.kpn.finobbot_io.exception.BotEnvironmentVariableNotExistException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.ApiContext;
@@ -11,21 +13,39 @@ import org.telegram.telegrambots.meta.ApiContext;
 @Configuration
 public class BotConfig {
 
-    @Value("${bot.name}")
-    private String name;
-    @Value("${bot.token}")
-    private String token;
-    @Value("${bot.webhookPath}")
-    private String webhookPath;
+    @Value("${env.var.names.bot.name}")
+    private String varBotName;
+    @Value("${env.var.names.bot.token}")
+    private String varBotToken;
+    @Value("${env.var.names.bot.webhookPath}")
+    private String varBotWebhook;
+
+    private final Environment environment;
+
+    public BotConfig(Environment environment) {
+        this.environment = environment;
+    }
 
     @Bean
-    public TelegramWebhookBot bot(){
+    public TelegramWebhookBot bot() throws BotEnvironmentVariableNotExistException {
+
+        String webhookPath = getBotEnvVar(varBotWebhook);
+        String name = getBotEnvVar(varBotName);
+        String token = getBotEnvVar(varBotToken);
 
         return new Bot(
                 ApiContext.getInstance(DefaultBotOptions.class),
                 webhookPath,
                 name,
-                webhookPath
+                token
         );
+    }
+
+    private String getBotEnvVar(String property) throws BotEnvironmentVariableNotExistException {
+        String value = environment.getProperty(property);
+        if (value == null){
+            throw new BotEnvironmentVariableNotExistException(property);
+        }
+        return value;
     }
 }
---
+++ finobBot-IO/src/main/java/org/kpn/finobbot_io/exception/BotEnvironmentVariableNotExistException.java
@@ -0,0 +1,7 @@
+package org.kpn.finobbot_io.exception;
+
+public class BotEnvironmentVariableNotExistException extends Exception{
+    public BotEnvironmentVariableNotExistException(String message) {
+        super(message);
+    }
+}
